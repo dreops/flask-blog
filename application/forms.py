7bcb6cf073e5809f22b2ce5d00af23374c4c235d
dreops <dreops@andreasandrews.com>
2020-03-13 17:25:14 +0000

    Up to stage 9 but with a potential issue that should be checked on the 'account.html' page

--- application/forms.py
+++ application/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from application.models import Users
+from flask_login import current_user
 
 class PostForm(FlaskForm):
 
@@ -74,3 +75,23 @@ class LoginForm(FlaskForm):
         remember = BooleanField('Remember Me')
 
         submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name',
+        validators = [
+            DataRequired(),
+            Length(min=4, max=30)
+        ]
+    )
+    last_name = StringField('Last Name',
+        validators = [
+            DataRequired(),
+            Email()
+        ])
+    submit = SubmitField('Update')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            user = Users.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already in use')
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,6 @@
 from application import app, db, bcrypt
 from flask import render_template, redirect, url_for, request
-from application.forms import PostForm, RegistrationForm, LoginForm
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 from application.models import Posts, Users
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -74,3 +74,18 @@ def post():
 def logout():
         logout_user()
         return redirect(url_for('login'))
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+        form = UpdateAccountForm()
+        if form.validate_on_submit():
+            current_user.first_name = form.first_name.data
+            current_user.last_name = form.email.data
+            db.session.commit()
+            return redirect(url_for('account'))
+        elif request.method == 'GET':
+            form.first_name.data = current_user.first_name
+            form.last_name.data = current_user.last_name
+            form.email.data = current_user.email
+        return render_template('account.html', title='Account', form=form)
